adfc1fb28f12477a70d579610ad829800c598292
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-06-05 13:34:38 -0300

    create and fail create tests ok

--- src/test/java/com/example/user_microservice/UserMicroserviceApplicationTests.java
+++ src/test/java/com/example/user_microservice/UserMicroserviceApplicationTests.java
@@ -1,13 +1,62 @@
 package com.example.user_microservice;
 
+import com.example.user_microservice.dtos.UserRecordDto;
+import com.example.user_microservice.models.UserModel;
+import com.example.user_microservice.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@WebMvcTest
 class UserMicroserviceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@MockBean
+	private UserService userService;
+
 	@Test
-	void contextLoads() {
+	void testCreateUserSuccess() throws Exception {
+		UserRecordDto userRecordDto = new UserRecordDto("Test user", "testuser@example.com");
+		UserModel userModel = new UserModel();
+		BeanUtils.copyProperties(userRecordDto, userModel);
+
+		//mocks the user doesnt already exists
+		Mockito.when(userService.getUserByEmail(userRecordDto.email())).thenReturn(null);
+
+		//mocks the success of saving user and returns its userModel
+		Mockito.when(userService.saveUser(Mockito.any(UserModel.class))).thenReturn(userModel);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/users")
+						.contentType("application/json")
+						.content(objectMapper.writeValueAsString(userRecordDto)))
+				.andExpect(MockMvcResultMatchers.status().isCreated())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value(userRecordDto.name()))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.email").value(userRecordDto.email()));
 	}
+
+	@Test
+	void testCreateUserFailure() throws Exception {
+		UserRecordDto userRecordDto = new UserRecordDto("Test user", "");
+		mockMvc.perform(MockMvcRequestBuilders.post("/users")
+						.contentType("application/json")
+						.content(objectMapper.writeValueAsString(userRecordDto)))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest());
+	}
+
 
 }
